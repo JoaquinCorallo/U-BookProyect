511d2b8f05ed871cc656581e1fe53cd2faaf85f5
Hugo Quintero <hugojquinterop@gmail.com>
2021-03-20 18:14:14 -0300

    move main activity

--- app/src/main/java/com/okbit/ubook/main/MainActivity.kt
+++ app/src/main/java/com/okbit/ubook/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.okbit.ubook
+package com.okbit.ubook.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.okbit.ubook.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

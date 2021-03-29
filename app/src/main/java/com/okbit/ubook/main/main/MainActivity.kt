383ceb254de4bf8d5ef0b262fef78ce68f8a49df
joaquinC <joaquin.corallo@outlook.com>
2021-03-28 22:12:14 -0300

    forms2

---
+++ app/src/main/java/com/okbit/ubook/main/main/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.okbit.ubook.main.main
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
+import com.okbit.ubook.R
+import com.okbit.ubook.main.upload.UploadActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.btnupload)
+            button.setOnClickListener {
+                val intent = Intent(this, UploadActivity::class.java)
+                startActivity(intent)
+            }
+
+
+        val switchchangetheme = findViewById<SwitchCompat>(R.id.switch_change_theme)
+
+        switchchangetheme.setTextOn("On")
+        switchchangetheme.setTextOff("Off")
+        switchchangetheme?.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                delegate.applyDayNight()
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                delegate.applyDayNight()
+
+            }
+        }
+    }
+}
--- app/src/main/java/com/okbit/ubook/main/upload/UploadActivity.kt
+++ app/src/main/java/com/okbit/ubook/main/upload/UploadActivity.kt
@@ -1,11 +1,56 @@
 package com.okbit.ubook.main.upload
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+import com.okbit.ubook.R
+
 
 class UploadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload)
+
+        val staticSpinner = findViewById<View>(R.id.tipoTransaccionSpinner) as Spinner
+
+        // Create an ArrayAdapter using the string array and a default spinner
+
+        // Create an ArrayAdapter using the string array and a default spinner
+        val staticAdapter = ArrayAdapter
+                .createFromResource(this, R.array.tipoTransaccionArray,
+                        android.R.layout.simple_spinner_item)
+
+        // Specify the layout to use when the list of choices appears
+
+        // Specify the layout to use when the list of choices appears
+        staticAdapter
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Apply the adapter to the spinner
+
+        // Apply the adapter to the spinner
+        staticSpinner.adapter = staticAdapter
+
+        val staticSpinner2 = findViewById<View>(R.id.condicionSpinner) as Spinner
+
+        // Create an ArrayAdapter using the string array and a default spinner
+
+        // Create an ArrayAdapter using the string array and a default spinner
+        val staticAdapter2 = ArrayAdapter
+                .createFromResource(this, R.array.condicionArray,
+                        android.R.layout.simple_spinner_item)
+
+        // Specify the layout to use when the list of choices appears
+
+        // Specify the layout to use when the list of choices appears
+        staticAdapter2
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Apply the adapter to the spinner
+
+        // Apply the adapter to the spinner
+        staticSpinner2.adapter = staticAdapter2
     }
 }

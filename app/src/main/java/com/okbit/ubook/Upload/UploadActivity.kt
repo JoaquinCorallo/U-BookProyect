4fa9d87098ad1cd0b3b5fa71145c48945c87725c
Joaquin Corallo <49536654+JoaquinCorallo@users.noreply.github.com>
2021-04-03 21:49:15 -0300

    Merge

---
+++ app/src/main/java/com/okbit/ubook/Upload/UploadActivity.kt
@@ -0,0 +1,150 @@
+package com.okbit.ubook.Upload
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.DialogInterface
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textfield.TextInputLayout
+import com.okbit.ubook.R
+import com.okbit.ubook.main.MainActivity
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+private var FILE_NAME = "Book" + SimpleDateFormat("yyyyMMdd_HHmmss_", Locale.getDefault()).format(Date())
+private const val REQUEST_CODE = 13
+private lateinit var photofile: File
+var isPhotoTaken = false
+
+class UploadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upload)
+
+        // Form values to control
+        val alertBookTitle = findViewById<TextInputLayout>(R.id.tituloInput)
+        val staticSpinner = findViewById<View>(R.id.tipoTransaccionSpinner) as Spinner
+        val staticSpinner2 = findViewById<View>(R.id.condicionSpinner) as Spinner
+        val controlLanguage = findViewById<TextInputLayout>(R.id.idiomaInput)
+        val controlAutor = findViewById<TextInputLayout>(R.id.autorInput)
+        val alertImageView = findViewById<ImageView>(R.id.photo)
+        val button = findViewById<Button>(R.id.agregarFoto)
+
+        // Floating Action Button
+        val actionButton = findViewById<FloatingActionButton>(R.id.addBook)
+        actionButton.setOnClickListener {
+            val alertBookThumbnail = (alertImageView.drawable)
+            val alertBookTitleText = alertBookTitle.editText?.text.toString()
+            val controlLanguageText = controlLanguage.editText?.text.toString()
+            val controlAutorText = controlAutor.editText?.text.toString()
+            val controlTipoTransaccionText = staticSpinner.selectedItem.toString()
+            val controlCondicionText = staticSpinner2.selectedItem.toString()
+            AlertDialog.Builder(this)
+                    .setTitle(alertBookTitleText)
+                    .setMessage("Estas seguro que deseas publicar este libro?")
+                    .setIcon(alertBookThumbnail)
+                    .setPositiveButton("Si", DialogInterface.OnClickListener { _, _ -> confirm(alertBookTitleText, controlLanguageText, controlAutorText, controlTipoTransaccionText, controlCondicionText, isPhotoTaken
+                    ) })
+                    .setNegativeButton("Volver a editar", null).show()
+        }
+
+        // Create an ArrayAdapter using the string array and a default spinner
+
+        // Create an ArrayAdapter using the string array and a default spinner
+        val staticAdapter = ArrayAdapter
+                .createFromResource(this, R.array.tipoTransaccionArray,
+                        android.R.layout.simple_spinner_item)
+
+        // Specify the layout to use when the list of choices appears
+
+        // Specify the layout to use when the list of choices appears
+        staticAdapter
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Apply the adapter to the spinner
+
+        // Apply the adapter to the spinner
+        staticSpinner.adapter = staticAdapter
+
+
+        // Create an ArrayAdapter using the string array and a default spinner
+
+        // Create an ArrayAdapter using the string array and a default spinner
+        val staticAdapter2 = ArrayAdapter
+                .createFromResource(this, R.array.condicionArray,
+                        android.R.layout.simple_spinner_item)
+
+        // Specify the layout to use when the list of choices appears
+
+        // Specify the layout to use when the list of choices appears
+        staticAdapter2
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Apply the adapter to the spinner
+
+        // Apply the adapter to the spinner
+        staticSpinner2.adapter = staticAdapter2
+
+        // Button to take pictures
+        button.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            photofile = getPhotoFile(FILE_NAME)
+
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photofile)
+            val fileProvider = FileProvider.getUriForFile(this, "com.okbit.ubook.fileprovider", photofile)
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
+            try {
+                startActivityForResult(takePictureIntent, REQUEST_CODE)
+                isPhotoTaken = true
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(applicationContext, "Camera not found", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun confirm(titleText: String, languajeText: String, autorText: String, tipoTransaccion: String, condicion: String, photo: Boolean) {
+        if (titleText != "" && languajeText != "" && autorText != "" && tipoTransaccion != "" && condicion != "" && photo) {
+            print("subo a la base de datos")
+            Toast.makeText(this@UploadActivity, "Libro cargado con exito", Toast.LENGTH_LONG).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        } else {
+            Toast.makeText(this@UploadActivity, "Debes llenar todos los campos obligatorios marcados con *", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun getPhotoFile(fileName: String): File {
+        val storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(fileName, ".jpg", storageDirectory)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            val takenImage = data?.extras?.get("data") as Bitmap
+            val fullImage = BitmapFactory.decodeFile(photofile.absolutePath)
+            print(fullImage)
+            print(photofile.absolutePath)
+            Log.d("Path de la image", photofile.absolutePath)
+            val imageView = findViewById<ImageView>(R.id.photo)
+            imageView.setImageBitmap(takenImage)
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+}
--- app/src/main/java/com/okbit/ubook/main/MainActivity.kt
+++ app/src/main/java/com/okbit/ubook/main/MainActivity.kt
@@ -3,15 +3,43 @@ package com.okbit.ubook.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import com.okbit.ubook.R
+import com.okbit.ubook.Upload.UploadActivity
 import com.okbit.ubook.crud.BookList
 import com.okbit.ubook.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val locale = Locale("es", "es")
+        Locale.setDefault(locale)
+
+        val button = findViewById<Button>(R.id.btn_upload)
+        button.setOnClickListener {
+            val intent = Intent(this, UploadActivity::class.java)
+            startActivity(intent)
+        }
+
+        val switchchangetheme = findViewById<SwitchCompat>(R.id.switch_change_theme)
+
+        switchchangetheme.setTextOn("On")
+        switchchangetheme.setTextOff("Off")
+        switchchangetheme?.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                delegate.applyDayNight()
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                delegate.applyDayNight()
+
+            }
+        }
 
         binding.listBtn.setOnClickListener {
             openBookList()

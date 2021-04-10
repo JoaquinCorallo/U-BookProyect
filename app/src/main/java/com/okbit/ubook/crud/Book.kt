4568d17b93ba9885b3737f6a89d5c2dee1bcfb39
Hugo Quintero <hugojquinterop@gmail.com>
2021-04-10 10:55:53 -0300

    Act Book.kt

--- app/src/main/java/com/okbit/ubook/crud/Book.kt
+++ app/src/main/java/com/okbit/ubook/crud/Book.kt
@@ -4,11 +4,15 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "books")
-data class Book(@PrimaryKey val title: String,
-                val cover: String,
+data class Book(@PrimaryKey val id: Int,
+                val title: String,
+                val cover: String, //base64
                 val author: String,
                 val category: String,
                 val description: String,
                 val condition: String,
                 val contact: String,
+                val price: Double,
+                val isbn: Int,
+                val language: String,
                 val delivery: String)
